b751e89614b93f9b9b2a62dd7eb4454a56d94b73
RodrigoFrancaBR <RodrigoFrancaBR@users.noreply.github.com>
2020-04-01 20:35:43 -0300

    Testes com clique de botão e msg com Toast.

--- conversor-de-moedas/app/src/main/java/com/rfranca/conversordemoedas/MainActivity.java
+++ conversor-de-moedas/app/src/main/java/com/rfranca/conversordemoedas/MainActivity.java
@@ -7,8 +7,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
@@ -16,7 +17,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         buscarElementosDaInterface();
-        exibirHelloWorld();
+        this.mViewHolder.buttonCalcular.setOnClickListener(this);
+        // onclick();
+        // exibirHelloWorld();
+    }
+
+//    public void onclick(View view) {
+//    }
+
+//    private void onclick() {
+//        this.mViewHolder.buttonCalcular.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
+//    }
+
+    @Override
+    public void onClick(View v) {
+        this.mViewHolder.editValue.setText(getString(R.string.clicou)+ mViewHolder.index + " vez.");
+        Toast.makeText(this, getString(R.string.clicou) + mViewHolder.index + " vez.", Toast.LENGTH_LONG).show();
+        mViewHolder.index +=1;
     }
 
     private void buscarElementosDaInterface() {
@@ -26,12 +48,13 @@ public class MainActivity extends AppCompatActivity {
         this.mViewHolder.buttonCalcular = findViewById(R.id.button_calcular);
     }
 
-
     private void exibirHelloWorld() {
         this.mViewHolder.editValue.setText("Hello World");
     }
 
+
     private static class ViewHolder {
+        int index = 0;
         EditText editValue;
         TextView textEuro;
         TextView textDolar;
