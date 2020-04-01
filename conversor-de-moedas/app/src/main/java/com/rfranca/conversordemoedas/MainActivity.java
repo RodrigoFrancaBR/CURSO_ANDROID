718e4d73c233eb19e99e11a10049e64ddaae0e6f
RodrigoFrancaBR <RodrigoFrancaBR@users.noreply.github.com>
2020-04-01 18:50:06 -0300

    Implementando o viewHolder.

--- conversor-de-moedas/app/src/main/java/com/rfranca/conversordemoedas/MainActivity.java
+++ conversor-de-moedas/app/src/main/java/com/rfranca/conversordemoedas/MainActivity.java
@@ -3,12 +3,38 @@ package com.rfranca.conversordemoedas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buscarElementosDaInterface();
+        exibirHelloWorld();
+    }
+
+    private void buscarElementosDaInterface() {
+        this.mViewHolder.editValue = findViewById(R.id.edit_valor);
+        this.mViewHolder.textDolar = findViewById(R.id.text_dolar);
+        this.mViewHolder.textEuro = findViewById(R.id.text_euro);
+        this.mViewHolder.buttonCalcular = findViewById(R.id.button_calcular);
+    }
+
+
+    private void exibirHelloWorld() {
+        this.mViewHolder.editValue.setText("Hello World");
+    }
+
+    private static class ViewHolder {
+        EditText editValue;
+        TextView textEuro;
+        TextView textDolar;
+        Button buttonCalcular;
     }
 }
