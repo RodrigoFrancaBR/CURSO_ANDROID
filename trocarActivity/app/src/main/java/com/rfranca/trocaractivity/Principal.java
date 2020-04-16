cddbf001c9a9a491bdea7a05fe0c1dee3edc5c38
RodrigoFrancaBR <RodrigoFrancaBR@users.noreply.github.com>
2020-04-15 23:03:04 -0300

    usando itentsImplicitas.

--- trocarActivity/app/src/main/java/com/rfranca/trocaractivity/Principal.java
+++ trocarActivity/app/src/main/java/com/rfranca/trocaractivity/Principal.java
@@ -2,6 +2,7 @@ package com.rfranca.trocaractivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -18,5 +19,16 @@ public class Principal extends Activity {
         Intent intent = new Intent(this, SegundaTela.class);
         startActivity(intent);
         //finish();
+    }
+
+    public void abrirLink(View v) {
+        Intent intentLink = new Intent();
+        // Definindo ação: VIEW (visualização)
+        intentLink.setAction(Intent.ACTION_VIEW);
+        // Definindo categoria: DAFEAULT (Padrão)
+        intentLink.addCategory(Intent.CATEGORY_DEFAULT);
+        // Definindo o endereço do link a ser aberto
+        intentLink.setData(Uri.parse("http://www.google.com"));
+        startActivity(intentLink);
     }
 }
